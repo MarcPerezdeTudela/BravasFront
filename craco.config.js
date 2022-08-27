0c65820f33b0b6ba5545c451be38b94c8af4bbe8
Marc Pérez de Tudela <mperezdetudelaaznar@gmail.com>
2022-08-27 09:42:45 +0200

    build: initial setup

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+const path = require(`path`)
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@types': path.resolve(__dirname, 'src/types'),
+    },
+  },
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+  margin: 0,
+  padding: 0,
+  boxSizing: 'border-box',
+}
